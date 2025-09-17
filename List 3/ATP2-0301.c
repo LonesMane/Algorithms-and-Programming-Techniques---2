412f6864d13fec4110a1d022cdc70cb8da8b5c23
Milko-Rosas <g.vieira26@unesp.br>
2025-09-17 20:58:41 -0300

    .

---
+++ List 3/ATP2-0301.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+struct livro{
+    char titulo[50];
+    char autor[50];
+    int ano;
+};
+
+int main()
+{
+    struct livro livro1;
+
+    printf("Digite o titulo do livro: ");
+    fgets(livro1.titulo, 50, stdin);
+    printf("\n\nDigite o autor do livro: ");
+    fgets(livro1.autor, 50, stdin);
+    printf("\n\nDigite o ano do livro: ");
+    scanf("%d", &livro1.ano);
+
+    printf("\n\nTitulo: %s", livro1.titulo);
+    printf("Autor: %s", livro1.autor);
+    printf("Ano: %d", livro1.ano);
+
+    return 0;
+}

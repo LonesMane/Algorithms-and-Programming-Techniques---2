8c0ea0d9b6b4109638e5b44e5037962ce57ded7b
Milko-Rosas <g.vieira26@unesp.br>
2025-10-29 04:13:35 -0300

    .

--- milko.h
+++ milko.h
@@ -7,4 +7,84 @@ void Milko (int repeticoes)
         printf("Eu te amo, Skie! S2\n");
 }
 
+int openR (void)
+{
+    char nome_arquivo[30];
+    printf("\n=== Lendo Arquivo ===\n");
+    printf("Nome: ");
+    nome_arquivo[strcspn(nome_arquivo, "\n")] = '\0';
+    fgets(nome_arquivo, 30, stdin);
+    FILE *arq;
+    arq = fopen (nome_arquivo, "r");
+    if (arq == NULL)
+    {
+        printf("\nErro ao criar o arquivo!\n");
+        printf("Encerrando...");
+        return 1;
+    } else {
+        printf("\n=== Arquivo Lido ===\n");
+        return 0;
+    }
+}
+
+int openW (void)
+{
+    char nome_arquivo[30];
+    printf("\n=== Criando Arquivo ===\n");
+    printf("Nome: ");
+    nome_arquivo[strcspn(nome_arquivo, "\n")] = '\0';
+    fgets(nome_arquivo, 30, stdin);
+    FILE *arq;
+    arq = fopen (nome_arquivo, "w");
+    if (arq == NULL)
+    {
+        printf("\nErro ao criar o arquivo!\n");
+        printf("Encerrando...");
+        return 1;
+    } else {
+        printf("\n=== Arquivo Criado ===\n");
+        return 0;
+    }
+}
+
+int openRB (void)
+{
+    char nome_arquivo[30];
+    printf("\n=== Lendo Arquivo ===\n");
+    printf("Nome: ");
+    nome_arquivo[strcspn(nome_arquivo, "\n")] = '\0';
+    fgets(nome_arquivo, 30, stdin);
+    FILE *arq;
+    arq = fopen (nome_arquivo, "rb");
+    if (arq == NULL)
+    {
+        printf("\nErro ao criar o arquivo!\n");
+        printf("Encerrando...");
+        return 1;
+    } else {
+        printf("\n=== Arquivo Lido ===\n");
+        return 0;
+    }
+}
+
+int openWB (void)
+{
+    char nome_arquivo[30];
+    printf("\n=== Lendo Arquivo ===\n");
+    printf("Nome: ");
+    nome_arquivo[strcspn(nome_arquivo, "\n")] = '\0';
+    fgets(nome_arquivo, 30, stdin);
+    FILE *arq;
+    arq = fopen (nome_arquivo, "wb");
+    if (arq == NULL)
+    {
+        printf("\nErro ao criar o arquivo!\n");
+        printf("Encerrando...");
+        return 1;
+    } else {
+        printf("\n=== Arquivo Lido ===\n");
+        return 0;
+    }
+}
+
 #endif
